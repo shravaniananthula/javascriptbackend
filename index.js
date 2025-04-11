ec3440c5384414adb40a17c40d84b77a1b810a08
Shravani Ananthula <shravani.ananthula@gmail.com>
2025-04-11 14:19:24 -0500

    updated index.js

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.get("/twitter", (req, res) => {
 app.get("/login", (req, res) => {
   res.send("<h1>please login at this website</h1>");
 });
-app.get("/youtube", (req, res) => {
-  res.send("<h2>chai aur code</h2>");
-});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
